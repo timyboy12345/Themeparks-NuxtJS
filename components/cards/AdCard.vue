9e0499e1416f4e6333e968f07928a07d82c46255
Tim Arendsen <t.j.m.arendsen@gmail.com>
2023-03-26 17:08:46 +0200

    Added a Client-Only tag to the AdCard

--- components/cards/AdCard.vue
+++ components/cards/AdCard.vue
@@ -1,22 +1,24 @@
 <template>
-  <card v-if="show" :sub-title="$t('general.ad')">
-    <script
-      async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3611207014341558"
-      crossorigin="anonymous"
-    ></script>
-    <!-- Themeparkplanner Default Block -->
-    <ins
-      class="adsbygoogle"
-      style="display: block"
-      data-ad-client="ca-pub-3611207014341558"
-      data-ad-slot="9200028200"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    ></ins>
-    <script>
-      ;(adsbygoogle = window.adsbygoogle || []).push({})
-    </script>
+  <card :sub-title="$t('general.ad')">
+    <client-only>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3611207014341558"
+        crossorigin="anonymous"
+      ></script>
+      <!-- Themeparkplanner Default Block -->
+      <ins
+        class="adsbygoogle"
+        style="display: block"
+        data-ad-client="ca-pub-3611207014341558"
+        data-ad-slot="9200028200"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>
+        ;(adsbygoogle = window.adsbygoogle || []).push({})
+      </script>
+    </client-only>
   </card>
 </template>
 
@@ -25,13 +27,5 @@ import Card from '@/components/cards/Card'
 export default {
   name: 'AdCard',
   components: { Card },
-  data() {
-    return {
-      show: false,
-    }
-  },
-  mounted() {
-    this.show = true
-  },
 }
 </script>
